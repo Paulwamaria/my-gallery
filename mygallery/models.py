31a0bb6ae56bddfc0e30f3643795c091d5bd3b84
Paulwamaria <paulwamaria@gmail.com>
2019-10-04 14:36:26 +0300

    Add tests for class ImagePost

--- mygallery/models.py
+++ mygallery/models.py
@@ -2,16 +2,35 @@ from django.db import models
 
 # Create your models here.
 class ImageCategory(models.Model):
-    category_name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.name
+
+    def save_imagecategory(self):
+        self.save()
 
 class ImageLocation(models.Model):
-    location_name = models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
 
-class Image(models.Model):
+    def __str__(self):
+        return self.name
+
+    def save_imagelocation(self):
+        self.save()
+
+class ImagePost(models.Model):
     my_image = models.ImageField(upload_to ='allImage/', default = 'allImage/default.jpg')
     image_name = models.CharField(max_length=60)
     image_description = models.TextField()
     created_on = models.DateTimeField(auto_now = True)
-    image_category = models.ForeignKey('ImageCategory', on_delete=models.CASCEADE)
-    image_location = models.ForeignKey(Location, on_delete = models.CASCADE)
+    image_category = models.ForeignKey('ImageCategory', on_delete=models.CASCADE)
+    image_location = models.ForeignKey(ImageLocation, on_delete = models.CASCADE)
+
+
+    def __str__(self):
+        return self.image_name
+
+
+    def save_imagepost(self):
+        self.save()
--- mygallery/tests.py
+++ mygallery/tests.py
@@ -1,7 +1,29 @@
 from django.test import TestCase
+from .models import ImagePost, ImageLocation, ImageCategory
 
 # Create your tests here.
-class TestImage(TestCase):
-    
+
+
+class TestImagePost(TestCase):
+
     def setUp(self):
-        self.new_image = Image()
+        self.new_category = ImageCategory(name='nature')
+        self.new_category.save_imagecategory()
+
+        self.new_location = ImageLocation(name="Aboretum")
+        self.new_location.save_imagelocation()
+
+        self.new_image = ImagePost(image_name='hacker', image_description='This guy is a real hacker',
+                               image_location=self.new_location, image_category=self.new_category)
+        self.new_image.save_imagepost()
+
+    def test_isinstance(self):
+        self.new_image = ImagePost(image_name='hacker', image_description='This guy is a real hacker', 
+                               image_location=self.new_location,image_category=self.new_category)
+        self.new_image.save_imagepost()
+        self.assertTrue(isinstance(self.new_image, ImagePost))
+
+    def tearDown(self):
+        ImageLocation.objects.all().delete()
+        ImageCategory.objects.all().delete()
+        ImagePost.objects.all().delete()
