86a184ea6b63db3e2ffd34853ec761b8b2b8f820
Paulwamaria <paulwamaria@gmail.com>
2019-10-04 17:06:32 +0300

    Add index view

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.models import ImagePost, ImageCategory, ImageLocation
 
 # Create your views here.
+
+def index(request):
+    posts = ImgePost.objects.all()
+
+    context = {
+        "posts":posts
+    }
+    return render(request, 'index.html',context)
